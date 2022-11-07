05ab5e456d9e0923fd9a60cb27e719c672e9d5bf
Simon <abiatar.ms@gmail.com>
2022-11-07 10:01:11 -0300

    added passenger models and views

--- airline/flights/models.py
+++ airline/flights/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 # Create your models here.
 class Airport(models.Model):
     code = models.CharField(max_length=3)
@@ -18,3 +19,10 @@ class Flight(models.Model):
         return f"{self.id}: {self.origin} to {self.destination}"
 
 
+class Passenger(models.Model):
+    first = models.CharField(max_length=64)
+    last = models.CharField(max_length=64)
+    flights = models.ManyToManyField(Flight, blank=True, related_name="passengers")  # blank=True means that a passenger has no flight
+
+    def __str__(self) -> str:
+        return f"{self.first} {self.last}"
