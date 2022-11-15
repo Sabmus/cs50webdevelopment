43e26b6e0b70ab1a81c1cff3737007c8a447dc7a
Simon <abiatar.ms@gmail.com>
2022-11-14 23:39:56 -0300

    working on project 3

--- project3/mail/mail/static/mail/inbox.js
+++ project3/mail/mail/static/mail/inbox.js
@@ -22,9 +22,37 @@ function compose_email() {
   document.querySelector('#compose-body').value = '';
 
   // submit form
-  /*document.querySelector('#compose-form').addEventListener('onsubmit', () => {
-    console.log(document.querySelector('#compose-subject').value);
-  })*/
+  document.querySelector('#compose-form').onsubmit = function() {
+    
+    // form data
+    let formData = {
+      recipients: document.querySelector('#compose-recipients').value,
+      subject: document.querySelector('#compose-subject').value,
+      body: document.querySelector('#compose-body').value
+    };
+
+    // sent to Django API
+    fetch('emails', {
+      method: 'post',
+      body: JSON.stringify(formData)
+    })
+    .then(response => {
+      if (response.ok){
+        return response.json();
+      } else {
+        throw new Error(response.status);
+      }
+    })
+    .then(json => {
+      console.log(json);
+      load_mailbox('sent');
+    })
+    .catch(error => console.log(error));
+
+    // do not submit the form
+    return false;
+  };
+
 }
 
 function load_mailbox(mailbox) {
@@ -35,4 +63,26 @@ function load_mailbox(mailbox) {
 
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+
+  // get data
+  user_ = document.querySelector('h2').value;
+
+  fetch(`emails/${mailbox}`, {user: user_})
+  .then(response => {
+    return response.json();
+  })
+  .then(json => {
+
+    console.log(json);
+    json.forEach(ele => {
+      let emailData = `<p class="ptag"><span class="sender">${ele.sender}</span> <a class="email" href="emails/${ele.id}">${ele.subject}</a> <span class="timestamp">${ele.timestamp}</span></p>`;
+      let div = document.createElement('div');
+      div.setAttribute('class', 'email_div')
+      div.innerHTML = `${emailData}`;
+      document.querySelector('#emails-view').append(div);
+    });
+
+  })
+  .catch(error => console.log(error));
+  
 }
